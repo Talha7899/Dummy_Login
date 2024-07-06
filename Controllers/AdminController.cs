126200122cf8d8fcf4751e927e45da388039c503
Zubair Qamsr <zatechnologies2012@gmail.com>
2024-07-06 11:04:18 +0500

    first commit

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dummy_Login_System.Controllers
+{
+	public class AdminController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Login()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Login(string email, string password)
+		{
+			if(email == "admin@gmail.com" && password == "12345")
+			{
+				return RedirectToAction("Index");
+			}else if (email == "user@gmail.com" && password == "12345")
+			{
+				return RedirectToAction("Index","Home");
+			}
+			else
+			{
+				ViewData["Message"] = "Invalid Email Or Password";
+				return View();
+			}
+		}
+
+	}
+}
